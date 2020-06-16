e7cb1ddaa5eac52bfb31cd27cb56507d81304e5b
Akmat <sirazhidinovakmatali@gmail.com>
2020-06-16 16:56:37 +0600

    Implemented line chart component

---
+++ src/components/line-chart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['chartData'],
+  mounted () {
+    this.renderChart({
+      labels: this.chartData.map((data) => data.date),
+      datasets: [
+        {
+          label: 'Infected',
+          borderColor: '#3333ff',
+          data: this.chartData.map((data) => data.confirmed),
+          fill: true
+        },
+        {
+          label: 'Deaths',
+          borderColor: '#f87979',
+          backgroundColor: 'rgba(255, 0, 0, 0.5)',
+          data: this.chartData.map((data) => data.deaths),
+          fill: true
+        }
+      ]
+    }, { responsive: true, maintainAspectRatio: false })
+  }
+}
+</script>
